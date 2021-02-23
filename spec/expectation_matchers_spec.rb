e58ed2c9deca774994a439854c1fe3c8dc2b50ba
hager <hgrreda@gmail.com>
2021-02-23 14:53:26 +0200

    truthiness matchers

--- spec/expectation_matchers_spec.rb
+++ spec/expectation_matchers_spec.rb
@@ -1,5 +1,6 @@
 describe 'Expectation Matchers' do
     describe 'equivalence matchers' do
+        # skip
         
         it 'will match loose equality with #eq' do
             a = "2 cats"
@@ -31,4 +32,29 @@ describe 'Expectation Matchers' do
             expect(b).to be(c)       
         end
     end
+
+    describe 'truthiness matchers' do
+        it ' will match true/false' do
+            expect( 1 < 2 ).to be(true) 
+            expect( 1 > 2 ).to be(false)
+            expect('foo').not_to be(true)  #the string is not exactly true
+            expect(nil).not_to be(false) #nil is not exactly false
+            expect(0).not_to be(false) # 0 is not exactly false
+        end
+        it ' will match truthy/falsey' do
+            expect( 1 < 2 ).to be_truthy
+            expect( 1 > 2 ).to be_falsey
+            expect('foo').to be_truthy  #any value is considered true
+            expect(nil).to be_falsey #nil is considered false
+            expect(0).not_to be_falsey # 0 is not exactly false
+        end
+
+        it 'will match nill' do
+            expect(nil).to be_nil
+            expect(nil).to be(nil)
+            expect(false).not_to be_nil
+            expect(0).not_to be_nil
+        end
+    end
+    
 end
